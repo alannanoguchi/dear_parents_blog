d0c4385dcd05ea6a94f8b6fab86b304ac7ca3138
Alanna <alanna.noguchi@students.makeschool.com>
2020-03-04 11:20:20 -0800

    bug

--- dear_parents_blog/settings.py
+++ dear_parents_blog/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dotenv # don't forget to add this to keep keys hidden
-dotenv.load_dotenv('.env') # add this too
+# don't forget to add these two to keep keys hidden
+import dotenv
+dotenv.load_dotenv('.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
